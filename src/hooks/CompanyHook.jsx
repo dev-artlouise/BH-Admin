4484200ea0fdf108ae35ea144b66ce53d96b8737
dennis <dennisfalasantosnatividad@gmail.com>
2024-09-10 23:07:44 +0800

    Compaany form edit, zustand hook installation

---
+++ src/hooks/CompanyHook.jsx
@@ -0,0 +1,78 @@
+import axios from 'axios';
+import { API_BASE_URL } from 'services/API';
+import { create } from 'zustand';
+import * as Yup from 'yup';
+
+const PATH = 'companies';
+const SINGULAR_PATH = 'company';
+
+const useCompanyHook = create((set) => ({
+  initialValues: { name: null, urlimage: null },
+  validationSchema: Yup.object({
+    name: Yup.string().required('Company name is required'),
+    urlimage: Yup.mixed()
+      .required('Image file is required') // Check if image file is not null
+      .test('fileSize', 'The file is too large', (value) => {
+        return value && value.size <= 2048 * 1024; // Max is 2MB in bytes [can be adjusted in the server]
+      })
+      .test('fileFormat', 'Unsupported File Format', (value) => {
+        return value && ['image/jpeg', 'image/png'].includes(value.type); // Validate file type || only JPEG and PNG files are supported
+      })
+  }),
+  setInitialValues: (field, value) =>
+    set((state) => ({
+      initialValues: {
+        ...state.initialValues,
+        [field]: value
+      }
+    })),
+
+  resetInitialValues: () => {
+    set(() => ({
+      initialValues: { name: null, urlimage: null }
+    }));
+  },
+
+  // GET COMPANIES
+  getCompanies: async () => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/${PATH}`);
+
+      return response.data;
+    } catch (error) {
+      throw new Error('Failed to fetch companies: ' + error.message);
+    }
+  },
+
+  // GET COMPANY
+  getCompany: async (id) => {
+    try {
+      const response = await axios.get(`${API_BASE_URL}/${SINGULAR_PATH}/${id}`);
+      set(() => ({
+        initialValues: response.data
+      }));
+    } catch (error) {
+      throw new Error('Failed to fetch companies: ' + error.message);
+    }
+  },
+
+  // CREATE COMPANY
+  createCompany: async () => {
+    const response = await axios.post(`${API_BASE_URL}/${PATH}`, formData);
+    return response.data;
+  },
+
+  // DELETE COMPANY
+  deleteCompany: async (id) => {
+    const response = await axios.delete(`${API_BASE_URL}/${PATH}/${id}`);
+    return response.data;
+  },
+
+  // UPDATE COMPANY
+  updateCompany: async (id) => {
+    const response = await axios.put(`${API_BASE_URL}/${SINGULAR_PATH}/${id}`);
+    return response.data;
+  }
+}));
+
+export default useCompanyHook;
--- src/layout/Companies-Section/CompanyForm.jsx
+++ src/layout/Companies-Section/CompanyForm.jsx
@@ -1,27 +1,16 @@
 import { useState, useRef } from 'react';
 
 import { useFormik } from 'formik';
-import * as Yup from 'yup';
+
 import { Grid, Stack, Snackbar, Alert, FormHelperText } from '@mui/material';
 import MUITextField from 'components/common/MUITextField';
 import MUIButton from 'components/common/MUIButton';
-import { createCompany } from 'services/companiesServices';
 import { useCustomMutation } from 'services/customMutation';
 import { companyStyles } from 'styles/companyStyles';
-import { CloudUploadOutlined } from '@mui/icons-material';
+import { CloudUploadOutlined, RestoreOutlined } from '@mui/icons-material';
 import { blueGrey } from '@mui/material/colors';
-
-const validationSchema = Yup.object({
-  name: Yup.string().required('Company name is required'),
-  urlimage: Yup.mixed()
-    .required('Image file is reequired') // Check if image file is not null
-    .test('fileSize', 'The file is too large', (value) => {
-      return value && value.size <= 2048 * 1024; // Max is 2MB in bytes [can be adjusted in the server]
-    })
-    .test('fileFormat', 'Unsupported File Format', (value) => {
-      return value && ['image/jpeg', 'image/png'].includes(value.type); // Validate file type || only JPEG and PNG files are supported
-    })
-});
+import useCompanyHook from 'hooks/CompanyHook';
+import { ClearOutlined } from '@ant-design/icons';
 
 const CompanyForm = () => {
   const [file, setFile] = useState(null);
@@ -29,14 +18,12 @@ const CompanyForm = () => {
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState('');
 
+  // Hooks
+  const { createCompany, initialValues, setInitialValues, resetInitialValues, validationSchema } = useCompanyHook();
+
   const formik = useFormik({
-    initialValues: {
-      name: '',
-      urlimage: null
-    },
-
-    validationSchema: validationSchema,
-
+    initialValues: initialValues,
+    validationSchema: validationSchema, // Validate inputs with Yum
     onSubmit: (values) => {
       const formData = new FormData();
       formData.append('name', values.name); // Append the text field 'name'
@@ -44,15 +31,18 @@ const CompanyForm = () => {
 
       // Now trigger the mutation
       createCompanyMutation(formData);
-    }
+    },
+    validateOnChange: true
   });
 
   const onSuccess = () => {
     setSnackbarMessage('Company submitted successfully!');
     setSnackbarOpen(true);
     formik.resetForm(); // Reset the form after submission
+    resetInitialValues();
     setFile(null); // Reset the file state
 
+    console.log(initialValues);
     if (fileInputRef.current) {
       fileInputRef.current.value = ''; // Clear the file input value
     }
@@ -93,6 +83,14 @@ const CompanyForm = () => {
     formik.setFieldValue('urlimage', selectedFile); // Set Formik field value for validation
   };
 
+  // Handle inputs
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+
+    formik.setFieldValue(name, value); // Set Formik field value for validation
+    setInitialValues(name, value);
+  };
+
   const handleCloseSnackbar = () => {
     setSnackbarOpen(false);
   };
@@ -106,8 +104,8 @@ const CompanyForm = () => {
               label="Company Name"
               name="name"
               placeholder="Enter Company Name"
-              value={formik.values.name}
-              onChange={formik.handleChange}
+              value={initialValues.name}
+              onChange={handleChangeInput}
               onBlur={formik.handleBlur}
               fullWidth
               error={formik.touched.name && Boolean(formik.errors.name)}
@@ -117,6 +115,9 @@ const CompanyForm = () => {
         </Grid>
 
         <Grid item xs={12}>
+          {initialValues.id && <img src={initialValues.urlimage} alt="img" width={'auto'} height={200} />}
+
+          {/* UPDATE */}
           <Stack spacing={1}>
             <div
               style={{
@@ -144,10 +145,12 @@ const CompanyForm = () => {
         </Grid>
 
         <Grid item xs={12}>
-          <MUIButton size={'large'} type={'submit'} variant={'contained'} color={'primary'} label={'Submit'} isLoading={isLoading} />
+          <MUIButton size={'large'} type={'submit'} variant={'contained'} color={'primary'} label={'Submit'} isLoading={isLoading} />{' '}
+          {/* <MUIButton size={'large'} variant={'outlined'} color={'warning'} label={'Reset form'} endIcon={<RestoreOutlined />} /> */}
         </Grid>
       </Grid>
 
+      {JSON.stringify(initialValues)}
       {/* Snackbar Notification */}
       <Snackbar open={snackbarOpen} autoHideDuration={4000} onClose={handleCloseSnackbar}>
         <Alert onClose={handleCloseSnackbar} severity={isError ? 'error' : 'success'}>
